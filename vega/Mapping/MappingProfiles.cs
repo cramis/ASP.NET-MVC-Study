188cc2ef98ab43db810db7dff026b1d72af6a5dd
박진억 <cramis@gmail.com>
2018-09-02 14:20:39 +0900

    Make api 구현

---
+++ vega/Mapping/MappingProfiles.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using AutoMapper;
+using vega.Models;
+using vega.Models.Resources;
+
+namespace vega.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Make, MakeResource>();
+            CreateMap<Model, ModelResource>();
+
+        }
+    }
+}
---
+++ vega/Migrations/20180902043553_SeedDataBase.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace vega.Migrations
+{
+    public partial class SeedDataBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert Into Makes (Name) Values('Make1')");
+            migrationBuilder.Sql("Insert Into Makes (Name) Values('Make2')");
+            migrationBuilder.Sql("Insert Into Makes (Name) Values('Make3')");
+
+            migrationBuilder.Sql("Insert Into Models (Name, MakeId) Values('Make1-ModelA', (select Id from Makes where name = 'Make1'))");
+            migrationBuilder.Sql("Insert Into Models (Name, MakeId) Values('Make1-ModelB', (select Id from Makes where name = 'Make1'))");
+            migrationBuilder.Sql("Insert Into Models (Name, MakeId) Values('Make1-ModelC', (select Id from Makes where name = 'Make1'))");
+
+            migrationBuilder.Sql("Insert Into Models (Name, MakeId) Values('Make2-ModelA', (select Id from Makes where name = 'Make2'))");
+            migrationBuilder.Sql("Insert Into Models (Name, MakeId) Values('Make2-ModelB', (select Id from Makes where name = 'Make2'))");
+            migrationBuilder.Sql("Insert Into Models (Name, MakeId) Values('Make2-ModelC', (select Id from Makes where name = 'Make2'))");
+
+            migrationBuilder.Sql("Insert Into Models (Name, MakeId) Values('Make3-ModelA', (select Id from Makes where name = 'Make3'))");
+            migrationBuilder.Sql("Insert Into Models (Name, MakeId) Values('Make3-ModelB', (select Id from Makes where name = 'Make3'))");
+            migrationBuilder.Sql("Insert Into Models (Name, MakeId) Values('Make3-ModelC', (select Id from Makes where name = 'Make3'))");
+
+
+
+
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Makes where Name in ('Make1','Make2','Make3')");
+            
+        }
+    }
+}
