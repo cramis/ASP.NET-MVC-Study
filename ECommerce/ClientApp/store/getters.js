92b5477cade3870265510faea470c0f4a453508f
박진억 <cramis@gmail.com>
2018-09-16 12:51:52 +0900

    카트 버튼(내용 요약 기능 추가) 변경

--- ECommerce/ClientApp/store/getters.js
+++ ECommerce/ClientApp/store/getters.js
@@ -3,4 +3,9 @@ export const shoppingCartTotal = state => {
       accumulator + cartItem.price * cartItem.quantity;
   
     return state.cart.reduce(reducer, 0);
-  };
+};
+
+export const shoppingCartItemCount = state => {
+    const reducer= (accumulator, cartItem) => accumulator + cartItem.quantity;
+    return state.cart.reduce(reducer, 0);
+};
