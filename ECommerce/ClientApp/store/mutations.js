0f2cda836ac56df45857dde1c2f2865c7b2a7346
박진억 <cramis@gmail.com>
2018-09-16 12:37:40 +0900

    카트 수량 변경 기능 추가

--- ECommerce/ClientApp/store/mutations.js
+++ ECommerce/ClientApp/store/mutations.js
@@ -7,9 +7,17 @@ export const updateProductQuantity = (state, index) => {
 let cartItem = Object.assign({}, state.cart[index]);
 cartItem.quantity++;
 
+// 기존의 내용을 삭제하고 새로운 내용으로 대체한다.
 state.cart.splice(index, 1, cartItem);
 };
 
 export const removeProductFromCart = (state, index) => {
   state.cart.splice(index, 1);
+};
+
+export const setProductQuantity = (state, payload) => {
+  let cartItem = Object.assign({}, state.cart[payload.index]);
+  cartItem.quantity = payload.quantity;
+
+  state.cart.splice(payload.index, 1, cartItem);
 };
